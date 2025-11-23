6428fb1b521e50591eb38dc41613ed24eda23ffb
Dragon X <dragonx@Dragons-MacBook-Pro.local>
2025-11-24 00:07:48 +0530

    updated

--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:basics/homepage/widgets/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,24 +21,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Demo'),
-        backgroundColor: Colors.blue,
-      ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
-    );
-  }
-}
