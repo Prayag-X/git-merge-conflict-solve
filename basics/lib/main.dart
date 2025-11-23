14de6d4fdfe4c10364c246f896abf5be2a2afd42
Dragon X <dragonx@Dragons-MacBook-Pro.local>
2025-11-23 23:34:58 +0530

    made a basic flutter app

---
+++ basics/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+        backgroundColor: Colors.blue,
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
--- example.py
+++
@@ -1,1 +0,0 @@
-print("hello-sourashish-aryan-sakshi-divyansh")
